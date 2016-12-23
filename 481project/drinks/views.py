a8dbfdfb8680020698a7bc976f68ab8e16dab417
Phil Sauvey <philip.sauvey@gmail.com>
2016-12-22 22:13:49 -0600

    0

--- 481project/drinks/views.py
+++ 481project/drinks/views.py
@@ -292,7 +292,7 @@ def populate(request):
 	ingsheet = book['Sheet2']
 	typesheet=book['Sheet3']
 	sheet = book['Sheet1']
-	for number in range(582,692):
+	for number in range(692,792):
 		type_key=ndb.Key(DrinkType,str(sheet.cell(row=number,column=4).value).encode())
 		a = Drink(parent=type_key)
 		a.drink_name=str(sheet.cell(row=number,column=1).value).encode()
