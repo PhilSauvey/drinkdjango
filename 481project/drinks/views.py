ff1ed57ba449dd5d33b3a9f29ac9867e491ad291
Phil Sauvey <philip.sauvey@gmail.com>
2016-12-22 21:07:56 -0600

    8

--- 481project/drinks/views.py
+++ 481project/drinks/views.py
@@ -11,9 +11,6 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from google.appengine.ext import ndb
 
-global index
-index=2
-
 def ing_check(data,owned):
 	for m in data:
 		if m.ing_name not in owned:
@@ -290,37 +287,37 @@ def createUser(request):
 		
 def populate(request):
 	import openpyxl
-	global index
 	from openpyxl import load_workbook
 	book = load_workbook('database.xlsx')
 	ingsheet = book['Sheet2']
 	typesheet=book['Sheet3']
 	sheet = book['Sheet1']
-	"""for i in range(1,typesheet.max_row+1):
-		t=DrinkType()
-		t.drink_type=str(typesheet.cell(row=i,column=1).value).encode()
-		t.put()"""
-	type_key=ndb.Key(DrinkType,str(sheet.cell(row=index,column=4).value).encode())
-	a = Drink(parent=type_key)
-	a.drink_name=str(sheet.cell(row=index,column=1).value).encode()
-	a.drink_glass=str(sheet.cell(row=index,column=7).value).encode()
-	a.drink_garnish=str(sheet.cell(row=index, column=5).value).encode()
-	a.drink_inst=str(sheet.cell(row=index, column=6).value).encode()
-	a.drink_slug=str(sheet.cell(row=index, column=8).value).encode()
-	drink_key=a.put()
-	for i in range(9,sheet.max_column+1):
-		if sheet.cell(row=index, column=i).value !=None:
-			n=Ingredients(parent=drink_key)
-			n.ing_name=str(sheet.cell(row=1, column=i).value).encode()
-			n.ing_amount=str(sheet.cell(row=index, column=i).value).encode()
-			n.put()
-	index += 1
-	"""
-	index=0
-	for k in range(1,ingsheet.max_row+1):
-		q= AllIngredients(ing_name=str(ingsheet.cell(row=k, column=1).value).encode())
-		q.index=index			
-		q.put()
-		index+=1
-	"""
+	if request.method="POST":
+		number=int(self.request.get("number")
+		type_key=ndb.Key(DrinkType,str(sheet.cell(row=number,column=4).value).encode())
+		a = Drink(parent=type_key)
+		a.drink_name=str(sheet.cell(row=number,column=1).value).encode()
+		a.drink_glass=str(sheet.cell(row=number,column=7).value).encode()
+		a.drink_garnish=str(sheet.cell(row=number, column=5).value).encode()
+		a.drink_inst=str(sheet.cell(row=number, column=6).value).encode()
+		a.drink_slug=str(sheet.cell(row=number, column=8).value).encode()
+		drink_key=a.put()
+		for i in range(9,sheet.max_column+1):
+			if sheet.cell(row=number, column=i).value !=None:
+				n=Ingredients(parent=drink_key)
+				n.ing_name=str(sheet.cell(row=1, column=i).value).encode()
+				n.ing_amount=str(sheet.cell(row=number, column=i).value).encode()
+				n.put()
+		
+	else:
+		for i in range(1,typesheet.max_row+1):
+			t=DrinkType()
+			t.drink_type=str(typesheet.cell(row=i,column=1).value).encode()
+			t.put()
+		index=0
+		for k in range(1,ingsheet.max_row+1):
+			q= AllIngredients(ing_name=str(ingsheet.cell(row=k, column=1).value).encode())
+			q.index=index			
+			q.put()
+			index+=1
 	return redirect('/populate')
