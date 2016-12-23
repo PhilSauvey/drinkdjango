c01d1c3b5e0f784d1a8e40d2045d9da48ba7af16
Phil Sauvey <philip.sauvey@gmail.com>
2016-12-22 22:20:12 -0600

    6

--- 481project/drinks/views.py
+++ 481project/drinks/views.py
@@ -290,23 +290,12 @@ def populate(request):
 	from openpyxl import load_workbook
 	book = load_workbook('database.xlsx')
 	ingsheet = book['Sheet2']
-	typesheet=book['Sheet3']
-	sheet = book['Sheet1']
-	for number in range(792,817):
-		type_key=ndb.Key(DrinkType,str(sheet.cell(row=number,column=4).value).encode())
-		a = Drink(parent=type_key)
-		a.drink_name=str(sheet.cell(row=number,column=1).value).encode()
-		a.drink_glass=str(sheet.cell(row=number,column=7).value).encode()
-		a.drink_garnish=str(sheet.cell(row=number, column=5).value).encode()
-		a.drink_inst=str(sheet.cell(row=number, column=6).value).encode()
-		a.drink_slug=str(sheet.cell(row=number, column=8).value).encode()
-		drink_key=a.put()
-		for i in range(9,sheet.max_column+1):
-			if sheet.cell(row=number, column=i).value !=None:
-				n=Ingredients(parent=drink_key)
-				n.ing_name=str(sheet.cell(row=1, column=i).value).encode()
-				n.ing_amount=str(sheet.cell(row=number, column=i).value).encode()
-				n.put()	
+	index=0
+	for k in range(1,ingsheet.max_row+1):
+		q= AllIngredients(ing_name=str(ingsheet.cell(row=k, column=1).value).encode())
+		q.index=index			
+		q.put()
+		index+=1
 	context={}	
 	response=render(request, "drinks/populate.html",context)
 	return response
