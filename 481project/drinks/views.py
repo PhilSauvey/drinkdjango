85e70c71f209a3ec60347a62a23c3934c86bd230
Phil Sauvey <philip.sauvey@gmail.com>
2017-01-27 12:46:29 -0600

    a

--- 481project/drinks/views.py
+++ 481project/drinks/views.py
@@ -199,7 +199,7 @@ def results(request):
 				owned_list.append(ingredients)
 		sType=request.POST["search_type"]
 	if len(owned_list)==0:
-		ing_list.sort(key=lambda x: x.index)
+		ing_list= sorted(ing_list.items(), key=operator.itemgetter(1))
 		return render(request, "drinks/search.html", {
 			"ing_list":ing_list,
 			"error_message": "You didn't select any ingredients.",})
